04cd878ee069ea2fd22afd8494de2ddaef40d813
alexismendozaa <afmendozaf@uce.edu.ec>
2025-05-14 17:51:26 -0500

    ✨ feat: Add routes for login.

---
+++ login-ms/routes/authRoutes.js
@@ -0,0 +1,40 @@
+const express = require('express');
+const { login } = require('../controllers/authController');
+
+const router = express.Router();
+
+/**
+ * @openapi
+ * /api/login:
+ *   post:
+ *     summary: Login de usuario
+ *     description: Autentica a un usuario con su correo electrónico y contraseña
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               email:
+ *                 type: string
+ *               password:
+ *                 type: string
+ *     responses:
+ *       200:
+ *         description: Token de autenticación generado
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 token:
+ *                   type: string
+ *       400:
+ *         description: Error de autenticación o usuario no encontrado
+ *       500:
+ *         description: Error en el servidor
+ */
+router.post('/login', login);
+
+module.exports = router;
